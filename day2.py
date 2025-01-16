277a8b232bc3e171bf382887c524acacfb7a3328
meran-ahsan <chiamahiru@gmail.com>
2025-01-16 10:04:55 +0600

    day2.update0

--- day2.py
+++ day2.py
@@ -1,1 +1,19 @@
 # wowie we made it to day 2
+
+#we can select a character of a string by using [] and putting in the index of the char we want as arg, this is also called subscripting
+print("hello"[4])
+
+#anything inside a string is a string even numbers so the code below will just concatenate the two strings
+print("123" + "345")
+
+#numbers outside a string is deemed as interger,below is a normal sum print 
+print(3999+670)
+
+#when writing large numbers we use commas such as 345,679,978 but in python we can just use underscored instead of commas
+print(123_456_789)
+
+#floooooats in py(anything with a decimal)
+print(3.14)
+
+#the key to conditionals the damn booleans
+print(True)
