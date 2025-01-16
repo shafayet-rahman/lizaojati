030d2307b307b6a8b71a0be68aaf7279efe18eeb
meran-ahsan <chiamahiru@gmail.com>
2025-01-16 13:20:14 +0600

    day3.update2

--- day3.py
+++ day3.py
@@ -5,3 +5,12 @@ if day == 'friday':
     print("weekend")
 else:
     print('work')
+    
+#logical operators in python = ( >,<,>=,<=,=,==,!= ) ik what this means os need to be descriptive
+
+#is the number odd or even program
+user_input = int(input("What's your selected number to check?\n"))
+if user_input % 2 == 0:
+    print(f"{user_input} is an even number.")
+else:
+    print(f"{user_input} is an odd number.")
