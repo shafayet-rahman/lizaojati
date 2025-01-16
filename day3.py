e74da898a89522729fb4fe7bf4e7b0e5c69ab0d3
meran-ahsan <chiamahiru@gmail.com>
2025-01-16 13:31:14 +0600

    day3.update3

--- day3.py
+++ day3.py
@@ -14,3 +14,15 @@ if user_input % 2 == 0:
     print(f"{user_input} is an even number.")
 else:
     print(f"{user_input} is an odd number.")
+
+#nested if/else statement using an amusement part roller coaster ticket entry
+print("Welcome to the rollercoaster, please elaborate with the questions to buy tickets")
+height = int(input("What's your height in cm?\n"))
+age = int(input("What's your age?\n"))
+if height >= 120:
+    if age <= 18:
+        print("The ticket price is 7$")
+    else:
+        print("The ticket price is 12$")
+else:
+    print("You're not tall enough to enter the rollercoaster sorry!")
