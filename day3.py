3b5b4110705934d354e37b9666804cb23376ac33
meran-ahsan <chiamahiru@gmail.com>
2025-01-16 13:07:51 +0600

    day3.update1

--- day3.py
+++ day3.py
@@ -1,1 +1,7 @@
 #wowieee broski got into day 3 dayum and you didn't stop crazy grind frfr(conditional statements,logical operators,code blocks, scope)
+#conditional statements in python
+day = 'friday'
+if day == 'friday':
+    print("weekend")
+else:
+    print('work')
