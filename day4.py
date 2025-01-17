bc97299d7ada3d6096b99d5025d7ff048b98d374
meran-ahsan <chiamahiru@gmail.com>
2025-01-17 14:04:50 +0600

    day4.update1

--- day4.py
+++ day4.py
@@ -1,1 +1,5 @@
 #WOWWWW bro's doing the work frfr(wifey effect) day4(randomisation and lists in python)
+#you must first import the random module
+import random
+#you must thn use the random module and use the methods given with it(the code below outputs a random number 1 - 10)
+random_int = random.randint(1,10)
