98b3092964280cd2a21e89a633a3b537f05d215a
meran-ahsan <chiamahiru@gmail.com>
2025-01-18 10:11:00 +0600

    day5.update1

--- day5.py
+++ day5.py
@@ -1,1 +1,6 @@
-#damn bro is grinding through the night frfr, so proud of bro()
+#damn bro is grinding through the night frfr, so proud of bro(python loops - for loops,ranges and code blocks)
+#a simple for loop
+fruits = ['peach','cherry','apple']
+for fruit in fruits:
+    print('Big ' + fruit + ' pie')
+    
