b090b9eed7bd35fb816e1ec0dc8345cf671d7c92
meran-ahsan <chiamahiru@gmail.com>
2025-01-18 10:16:13 +0600

    day5.update2

--- day5.py
+++ day5.py
@@ -1,5 +1,5 @@
 #damn bro is grinding through the night frfr, so proud of bro(python loops - for loops,ranges and code blocks)
-#a simple for loop
+#a simple for loop(takes a var called fruit and uses it to iterate all of the items in the list)
 fruits = ['peach','cherry','apple']
 for fruit in fruits:
     print('Big ' + fruit + ' pie')
